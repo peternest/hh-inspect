2ce0649619e3d5d7e6de56676617e93ddf68347e
Peter Huggy <peter.huggy@gmail.com>
2025-05-28 15:13:06 +0300

    Add common functions to utils.py and tests

--- src/hh_inspect/utils.py
+++ src/hh_inspect/utils.py
@@ -1,5 +1,31 @@
 import re
-from typing import Final
+from collections import Counter
+from collections.abc import Iterable
+from typing import Any, Final
+
+
+def find_top_words_in_list(iterable: Iterable[str]) -> list[tuple[str, int]]:
+    words_counter: Final = Counter(iterable)
+    return sorted(words_counter.items(), key=lambda x: x[1], reverse=True)
+
+
+def get_field_value(obj: dict[str, Any], field1: str, field2: str) -> str:
+    """Return the value of obj.field1.field2 if it exists and "" otherwise.
+
+    Obj can be:
+        {"id": 123}
+        {"id": 123, "main": None}
+        {"id": 123, "main": {"abc": "def"}}
+        {"id": 123, "main": {"subname": None}}
+        {"id": 123, "main": {"subname": "normal_string"}}
+    """
+    dict_or_none = obj.get(field1)
+    if dict_or_none is None:
+        return ""
+    value_or_none = dict_or_none.get(field2, "")
+    if value_or_none is None:
+        return ""
+    return str(value_or_none)
 
 
 def remove_html_tags(html_text: str) -> str:
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,37 @@
+from collections.abc import Iterable
+from typing import Any
+
 import pytest
 
-from hh_inspect.utils import remove_html_tags
+from hh_inspect.utils import find_top_words_in_list, get_field_value, remove_html_tags
+
+
+@pytest.mark.parametrize(
+    ("iterable", "result"),
+    [
+        ([], []),
+        (["str"], [("str", 1)]),
+        (["a", "a", "a"], [("a", 3)]),
+        (["a", "b", "b", "b"], [("b", 3), ("a", 1)]),
+        (["a", "b", "b", "a", "a", "b"], [("a", 3), ("b", 3)]),
+    ],
+)
+def test_find_top_words_in_list(iterable: Iterable[str], result: list[tuple[str, int]]) -> None:
+    assert find_top_words_in_list(iterable) == result
+
+
+@pytest.mark.parametrize(
+    ("obj", "field1", "field2", "result"),
+    [
+        ({"id": 123}, "main", "subname", ""),
+        ({"id": 123, "main": None}, "main", "subname", ""),
+        ({"id": 123, "main": {"abc": "def"}}, "main", "subname", ""),
+        ({"id": 123, "main": {"subname": None}}, "main", "subname", ""),
+        ({"id": 123, "main": {"subname": "normal_string"}}, "main", "subname", "normal_string"),
+    ],
+)
+def test_get_field_value(obj: dict[str, Any], field1: str, field2: str, result: str) -> None:
+    assert get_field_value(obj, field1, field2) == result
 
 
 @pytest.mark.parametrize(
