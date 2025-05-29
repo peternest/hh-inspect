0ea0803a37f11b33223542e017f163abf1507bb0
Peter Huggy <peter.huggy@gmail.com>
2025-05-29 10:23:07 +0300

    Refactor analyzer.py

--- src/hh_inspect/analyzer.py
+++ src/hh_inspect/analyzer.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Final, Callable, Iterable
+from typing import Final, Iterable
 import re
 
 import pandas as pd
@@ -24,26 +24,31 @@ class Analyzer:
         self.working_df.to_csv(filename, index=False)
 
     def analyze_salary(self) -> None:
-        df: Final = self.working_df[["employer_name", "vacancy_name", "salary_from", "salary_to"]]
-        # print(f"\n{df.describe()}")
-
-        def print_salary_stat(prefix: str, field_name: str) -> None:
-            min_salary: float = df[field_name].min()  # type: ignore
-            max_salary: float = df[field_name].max()  # type: ignore
-            mean_salary: float = df[field_name].mean()  # type: ignore
-            median_salary: float = df[field_name].median()  # type: ignore
-            printer.print(
-                f"{prefix} min: {min_salary}, max: {max_salary}, mean: {mean_salary:.0f}, median: {median_salary:.0f}"
-            )
-
         printer.print("")
-        print_salary_stat("SALARY FROM", "salary_from")
-        print_salary_stat("SALARY TO", "salary_to")
+        self._print_salary_stat("SALARY FROM", "salary_from")
+        self._print_salary_stat("SALARY TO", "salary_to")
+
+    def _print_salary_stat(self, prefix: str, field_name: str) -> None:
+        """Print salary statistics for a given field."""
+
+        if field_name not in self.working_df:
+            logger.warning(f"Field '{field_name}' not found in working_df.")
+            return
+
+        df_column: Final = self.working_df[field_name]  # type: ignore
+
+        min_salary: float = df_column.min()  # type: ignore
+        max_salary: float = df_column.max()  # type: ignore
+        mean_salary: float = df_column.mean()  # type: ignore
+        median_salary: float = df_column.median()  # type: ignore
+        printer.print(
+            f"{prefix} min: {min_salary}, max: {max_salary}, mean: {mean_salary:.0f}, median: {median_salary:.0f}"
+        )
 
     def analyze_key_skills(self, print_amount: int = 10) -> None:
-        df: Final = self.working_df
+        df_column: Final = self.working_df["key_skills"]  # type: ignore
 
-        key_skills_list: list[list[str]] = df["key_skills"].to_list()  # type: ignore
+        key_skills_list: list[list[str]] = df_column.to_list()  # type: ignore
         skills_list: Final = [x for elem in key_skills_list for x in elem]
         top_skills: Final = find_top_words_in_list(skills_list)
 
@@ -52,18 +57,18 @@ class Analyzer:
             printer.print(f"{key[:20]:20} {value}")
 
     def analyze_description(self, print_amount: int = 15) -> None:
-        df: Final = self.working_df
+        df_column: Final = self.working_df["description"]  # type: ignore
 
-        noise_words: Final = set(["API", "IT", "quot", "and", "or", "I", "it"])
-
-        def filter_strings(filter_func: Callable[[str], bool], string_list: list[str]) -> Iterable[str]:
-            return filter(filter_func, string_list)
-
-        words_list: Final = " ".join(df["description"].to_list())  # type: ignore
+        words_list: Final = " ".join(df_column.to_list())  # type: ignore
         eng_words_list: Final[list[str]] = re.findall("[a-zA-Z_]+", words_list)
-        filtered: Final = filter_strings(lambda w: w not in noise_words, eng_words_list)
-        top_skills: Final = find_top_words_in_list(filtered)
+        filtered_list: Final = Analyzer.filter_noise_words(eng_words_list)
+        top_skills: Final = find_top_words_in_list(filtered_list)
 
         printer.print(f"\nThe {print_amount} most frequently used words in Description:")
         for key, value in top_skills[:print_amount]:
             printer.print(f"{key[:20]:20} {value}")
+
+    @staticmethod
+    def filter_noise_words(string_list: list[str]) -> Iterable[str]:
+        noise_words: Final = set(["API", "IT", "quot", "and", "or", "I", "it"])
+        return filter(lambda w: w not in noise_words, string_list)
